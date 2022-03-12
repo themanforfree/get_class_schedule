f40045e999a7d5360440aab6ae1dd4d4443074a0
themanforfree <themanforfree@gmail.com>
2022-03-12 11:27:19 +0800

    init

---
+++ src/jwglxt.rs
@@ -0,0 +1,109 @@
+use error_chain::error_chain;
+use rand::rngs::OsRng;
+use reqwest::Client;
+use rsa::{BigUint, PaddingScheme, PublicKey, RsaPublicKey};
+use serde_json::Value;
+error_chain! {
+    foreign_links {
+        ReqwestError(reqwest::Error);
+        RsaError(rsa::errors::Error);
+        Base64Error(base64::DecodeError);
+        SerdeJsonError(serde_json::Error);
+    }
+}
+pub struct STU {
+    username: String,
+    password: String,
+    client: Client,
+}
+
+impl STU {
+    pub fn new(username: &str, password: &str) -> STU {
+        STU {
+            username: username.to_string(),
+            password: password.to_string(),
+            client: Client::builder()
+                .cookie_store(true)
+                .build()
+                .expect("build client error"),
+        }
+    }
+
+    fn rsa_encode(pwd: &str, n: &str, e: &str) -> Result<String> {
+        let rsa_n = BigUint::from_bytes_be(&base64::decode(n)?);
+        let rsa_e = BigUint::from_bytes_be(&base64::decode(e)?);
+        let key = RsaPublicKey::new(rsa_n, rsa_e)?;
+        Ok(base64::encode(&key.encrypt(
+            &mut OsRng,
+            PaddingScheme::new_pkcs1v15_encrypt(),
+            pwd.as_bytes(),
+        )?))
+    }
+
+    async fn get_csrftoken(&self) -> Result<String> {
+        let res = self
+            .client
+            .get("https://jwglxt.haut.edu.cn/jwglxt/xtgl/login_slogin.html")
+            .send()
+            .await?
+            .text()
+            .await?;
+        let csrftoken = res
+            .split(r#"name="csrftoken" value=""#)
+            .nth(1)
+            .ok_or("csrftoken not found")?
+            .split(r#"""#)
+            .next()
+            .ok_or("csrftoken not found")?;
+        Ok(String::from(csrftoken))
+    }
+
+    pub async fn login(&self) -> Result<()> {
+        let csrftoken = self.get_csrftoken().await?;
+        let res = self
+            .client
+            .get("https://jwglxt.haut.edu.cn/jwglxt/xtgl/login_getPublicKey.html")
+            .send()
+            .await?
+            .text()
+            .await?;
+        let parsed: Value = serde_json::from_str(&res)?;
+        let n = parsed["modulus"].as_str().ok_or("modulus not found")?;
+        let e = parsed["exponent"].as_str().ok_or("exponent not found")?;
+
+        let res = self
+            .client
+            .post("https://jwglxt.haut.edu.cn/jwglxt/xtgl/login_slogin.html")
+            .form(&[
+                ("csrftoken", &csrftoken),
+                ("yhm", &self.username),
+                ("mm", &STU::rsa_encode(&self.password, &n, &e)?),
+                ("mm", &STU::rsa_encode(&self.password, &n, &e)?),
+            ])
+            .send()
+            .await?
+            .text()
+            .await?;
+        if res.contains("修改密码") {
+            Ok(())
+        } else {
+            Err("Wrong password or username".into())
+        }
+    }
+
+    pub async fn get_schedules(&self, xnm: u32, term: u32) -> Result<String> {
+        let xqm = match term {
+            1 => "3",
+            2 => "12",
+            _ => Err("term must be 1 or 2")?,
+        };
+        Ok(self
+            .client
+            .post("https://jwglxt.haut.edu.cn/jwglxt/kbcx/xskbcx_cxXsKb.html?gnmkdm=N2151")
+            .form(&[("xnm", xnm.to_string().as_str()), ("xqm", xqm)])
+            .send()
+            .await?
+            .text()
+            .await?)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,201 @@
+use clap::{Arg, Command};
+use csv;
+use error_chain::error_chain;
+use jwglxt::STU;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::fs::File;
+mod jwglxt;
+use chrono::{Duration, Local, TimeZone};
+error_chain!(
+    foreign_links {
+        SerdeJsonError(serde_json::Error);
+        IoError(std::io::Error);
+        CsvError(csv::Error);
+    }
+);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Class {
+    kcmc: String,
+    xm: String,
+    cdmc: String,
+    jcs: String,
+    zcd: String,
+    xqj: String,
+}
+
+impl Class {
+    fn empty() -> Class {
+        Class {
+            kcmc: String::new(),
+            xm: String::new(),
+            cdmc: String::new(),
+            jcs: String::new(),
+            zcd: String::new(),
+            xqj: String::new(),
+        }
+    }
+
+    fn to_records(&self) -> Vec<Vec<String>> {
+        const CLASS_LIST: [[&str; 2]; 8] = [
+            ["8:30", "9:15"],
+            ["9:25", "10:05"],
+            ["10:25", "11:10"],
+            ["11:20", "12:00"],
+            ["14:30", "15:15"],
+            ["15:25", "16:05"],
+            ["16:25", "17:10"],
+            ["17:20", "18:00"],
+        ];
+        let mut records = Vec::new();
+        let start = Local.ymd(2022, 2, 21);
+        let place = self
+            .cdmc
+            .replace("莲4号教学楼", "4")
+            .replace("文科组团楼", "3");
+
+        let time = self
+            .jcs
+            .split("-")
+            .map(|v| v.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        let weeks = self
+            .zcd
+            .replace("周", "")
+            .split("-")
+            .map(|v| v.parse::<i64>().unwrap())
+            .collect::<Vec<_>>();
+        let day = self.xqj.parse::<i64>().unwrap();
+        for week in weeks[0]..weeks[1] + 1 {
+            let date = (start + Duration::weeks(week - 1) + Duration::days(day - 1))
+                .format("%m/%d/%Y")
+                .to_string();
+
+            let start_time = CLASS_LIST[time[0] as usize - 1][0];
+            let end_time = CLASS_LIST[time[1] as usize - 1][1];
+            let record = vec![
+                self.kcmc.clone(),
+                date.clone(),
+                date,
+                start_time.to_string(),
+                end_time.to_string(),
+                "False".to_string(),
+                self.xm.clone(),
+                place.clone(),
+                "False".to_string(),
+            ];
+            records.push(record);
+        }
+        records
+    }
+}
+
+fn get_csv(schedules: &str) -> Result<()> {
+    let file = File::create("schedules.csv")?;
+    let mut writer = csv::Writer::from_writer(file);
+    writer.write_record(&[
+        "Subject",
+        "Start Date",
+        "End Date",
+        "Start Time",
+        "End Time",
+        "All Day Event",
+        "Description",
+        "Location",
+        "Private",
+    ])?;
+    let schedules: Value = serde_json::from_str(schedules)?;
+    for v in schedules["kbList"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|v| {
+            serde_json::from_value(v.clone()).unwrap_or_else(|e| {
+                eprintln!("{}", e);
+                Class::empty()
+            })
+        })
+        .map(|v| v.to_records())
+        .flatten()
+    {
+        writer.write_record(&v)?;
+    }
+    const CHINESE_NUM: [&str; 11] = [
+        "零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十",
+    ];
+    let start = Local.ymd(2022, 2, 21);
+    for i in 1..19 {
+        writer.write_record(&[
+            format!("第{}周", {
+                if i <= 10 {
+                    format!("{}", CHINESE_NUM[i as usize])
+                } else {
+                    format!("{}{}", CHINESE_NUM[10], CHINESE_NUM[i as usize - 10])
+                }
+            }),
+            (start + Duration::weeks(i - 1))
+                .format("%m/%d/%Y")
+                .to_string(),
+            (start + Duration::weeks(i)).format("%m/%d/%Y").to_string(),
+            String::new(),
+            String::new(),
+            String::from("True"),
+            String::new(),
+            String::new(),
+            String::from("False"),
+        ])?;
+    }
+    writer.flush()?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let matches = Command::new("jwglxt")
+        .version("0.1")
+        .author("themanforfree <themanforfree@gmail.com>")
+        .about("get haut class schedule")
+        .arg(
+            Arg::new("username")
+                .short('u')
+                .long("username")
+                .value_name("USERNAME")
+                .help("username of the system")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("password")
+                .short('p')
+                .long("password")
+                .value_name("PASSWORD")
+                .help("password of the system")
+                .takes_value(true),
+        )
+        .get_matches();
+    let username = match matches.value_of("username") {
+        Some(v) => v,
+        None => {
+            println!("please input username");
+            std::process::exit(1);
+        }
+    };
+    let password = match matches.value_of("password") {
+        Some(v) => v,
+        None => {
+            println!("please input password");
+            std::process::exit(1);
+        }
+    };
+    let stu = STU::new(username, password);
+    if let Err(e) = stu.login().await {
+        println!("Login Error: {}", e);
+        return;
+    }
+    let schedules = stu.get_schedules(2021, 2).await.unwrap();
+    if let Err(e) = get_csv(&schedules) {
+        println!("CSV Error: {}", e);
+        return;
+    }
+    println!("Done!");
+}
